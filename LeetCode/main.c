e5cf1ff6c5832daa263bd610b2c5642d34d677c0
刘楠 <liunan.coulson@bytedance.com>
2018-06-25 12:45:47 +0800

    无重复字符的最长子串

--- LeetCode/main.c
+++ LeetCode/main.c
@@ -7,12 +7,11 @@
 //
 
 #include <stdio.h>
-#include "problem1.h"
+#include "problem3.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int a[] = {3,2,4};
-    int *r = twoSum(&a, 3, 6);
-    printf("%d %d",r[0], r[1]);
+    char* s = "tmmzuxt";
+    printf("%d\n",lengthOfLongestSubstring(s));
     return 0;
 }
--- LeetCode/problem3.c
+++ LeetCode/problem3.c
@@ -8,4 +8,24 @@
 
 #include "problem3.h"
 
-int lengthOfLongestSubstring(char* s);
+int lengthOfLongestSubstring(char* s) {
+    int map[10000];
+    for (int i = 0; i < 10000 ; i++) {
+        map[i] = -1;
+    }
+    int len = (int)strlen(s);
+    int totalLength = 0;
+    for (int i = 0, curLength = 0; i < len ; i++) {
+        int pos = s[i];
+        if (map[pos] == -1 || i - map[pos] > curLength) {
+            curLength ++;
+            if (curLength > totalLength) {
+                totalLength = curLength;
+            }
+        } else {
+            curLength = i - map[pos];
+        }
+        map[pos] = i;
+    }
+    return totalLength;
+}
