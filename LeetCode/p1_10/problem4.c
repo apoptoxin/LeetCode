3a425dda0a33d4bda17d1e2667da01c2d69ef3fe
刘楠 <liunan.coulson@bytedance.com>
2018-06-25 16:13:55 +0800

    目录结构迁移

---
+++ LeetCode/p1_10/problem4.c
@@ -0,0 +1,9 @@
+//
+//  problem4.c
+//  LeetCode
+//
+//  Created by 刘楠 on 2018/6/25.
+//  Copyright © 2018年 micronil.com. All rights reserved.
+//
+
+#include "problem4.h"
---
+++ LeetCode/p1_10/problem4.h
@@ -0,0 +1,33 @@
+//
+//  problem4.h
+//  LeetCode
+//
+//  Created by 刘楠 on 2018/6/25.
+//  Copyright © 2018年 micronil.com. All rights reserved.
+//
+
+#ifndef problem4_h
+#define problem4_h
+
+#include <stdio.h>
+
+#endif /* problem4_h */
+
+//给定两个大小为 m 和 n 的有序数组 nums1 和 nums2 。
+//
+//请找出这两个有序数组的中位数。要求算法的时间复杂度为 O(log (m+n)) 。
+//
+//示例 1:
+//
+//nums1 = [1, 3]
+//nums2 = [2]
+//
+//中位数是 2.0
+//示例 2:
+//
+//nums1 = [1, 2]
+//nums2 = [3, 4]
+//
+//中位数是 (2 + 3)/2 = 2.5
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size);
