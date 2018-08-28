561780b224eac76369c80b1f55489da9269fe71b
刘楠 <liunan.coulson@bytedance.com>
2018-08-28 13:18:38 +0800

    50 二分法解决

--- LeetCode/p41_50/problem50.c
+++ LeetCode/p41_50/problem50.c
@@ -7,3 +7,27 @@
 //
 
 #include "problem50.h"
+
+//输入: 2.00000, -2
+//输出: 0.25000
+//解释: 2-2 = 1/22 = 1/4 = 0.25
+//说明:
+//
+//-100.0 < x < 100.0
+//n 是 32 位有符号整数，其数值范围是 [−231, 231 − 1] 。
+
+double myPow(double x, int n) {
+    if (n == 0) {
+        return 1.0;
+    } else if (n == 1) {
+        return x;
+    } else if (n == -1) {
+        return 1.0/x;
+    }
+    double half = myPow(x, n/2);
+    if (n % 2 != 0) {
+        return half * half * ((n < 0) ? 1.0/x : x);
+    } else {
+        return half * half;
+    }
+}
