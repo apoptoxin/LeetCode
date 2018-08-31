73cac84b1caf7bb31014cc26776827562b692b98
刘楠 <liunan.coulson@bytedance.com>
2018-08-31 15:50:33 +0800

    49 solved

--- LeetCode/main.c
+++ LeetCode/main.c
@@ -8,7 +8,7 @@
 
 #include <stdio.h>
 #include "sort.h"
-#include "problem47.h"
+#include "problem49.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -21,46 +21,46 @@ int main(int argc, const char * argv[]) {
 //        }
 //        s[i] = t;
 //    }
-//    char ori[6][3] = {{'e','a','t'}, {'t','e','a'}, {'t','a','n'},{'a','t','e'},{'n','a','t'},{'b','a','t'}};
-//    char **s = malloc(sizeof(char *)*9);
-//    for (int i = 0; i < 6; i++) {
-//        char *t = malloc(sizeof(char)*3);
-//        for (int j = 0; j<3; j++) {
-//            t[j] = ori[i][j];
-//        }
-//        s[i] = t;
-//    }
-//    int c = 0;
-//    int b = 0;
-//    int *p = &c;
-//    groupAnagrams(s,6,&p,&b);
+    char ori[6][3] = {{'e','a','t'}, {'t','e','a'}, {'t','a','n'},{'a','t','e'},{'n','a','t'},{'b','a','t'}};
+    char **s = malloc(sizeof(char *)*9);
+    for (int i = 0; i < 6; i++) {
+        char *t = malloc(sizeof(char)*3);
+        for (int j = 0; j<3; j++) {
+            t[j] = ori[i][j];
+        }
+        s[i] = t;
+    }
+    int c = 0;
+    int b = 0;
+    int *p = &c;
+    groupAnagrams(s,6,&p,&b);
 //    solveSudoku(s, 9, 9);
-//    for (int i = 0 ; i < 9; i++) {
-//        for (int j = 0; j < 9; j++) {
-//            printf("%c ",s[i][j]);
-//        }
-//        printf("\n");
-//    }
-    int a[] = {-1,2,0,-1,1,0,1};
+    for (int i = 0 ; i < b; i++) {
+        for (int j = 0; j < p[i]; j++) {
+            printf("%s ",s[i][j]);
+        }
+        printf("\n");
+    }
+//    int a[] = {-1,2,0,-1,1,0,1};
 //    int *p = malloc(sizeof(a)/sizeof(int));
 //    int size = 0;
 //    for (int i = 0 ; i < sizeof(a)/sizeof(int) ; i++) {
 //        p[i] = a[i];
 //        size++;
 //    }
-    int returnSize = 0;
-    int numsSize = sizeof(a) / sizeof(int);
-    int ** colS = permuteUnique(a, numsSize, &returnSize);
+//    int returnSize = 0;
+//    int numsSize = sizeof(a) / sizeof(int);
+//    int ** colS = permuteUnique(a, numsSize, &returnSize);
 //    int re = 0;
 //    int total = 0;
 //    int **r = permute(a, sizeof(a)/sizeof(int), &total);
 //    printf("%d\n",jump(a, sizeof(a)/sizeof(int)));
-    for (int i = 0 ; i < returnSize ; i++) {
-        for (int j = 0 ; j < numsSize; j++) {
-            printf("%d ",colS[i][j]);
-        }
-        printf("\n");
-    }
+//    for (int i = 0 ; i < returnSize ; i++) {
+//        for (int j = 0 ; j < numsSize; j++) {
+//            printf("%d ",colS[i][j]);
+//        }
+//        printf("\n");
+//    }
     
 //    printf("\ncount:%f\n",myPow(34.00515, -3));
 //    printf("\nre:%f\n",pow(34.00515,-3));
--- LeetCode/p41_50/problem49.c
+++ LeetCode/p41_50/problem49.c
@@ -9,75 +9,85 @@
 #include "problem49.h"
 #include <stdlib.h>
 
-int cmpfunc49str(const void * a, const void * b)
-{
-    return ( *(char*)a - *(char*)b );
-}
-
-int cmpfunc49Node (const void * a, const void * b)
-{
-    char **t1 = a;
-    char **t2 = b;
-    char *s1 = malloc(sizeof(char) * strlen(*t1));
-    char *s2 = malloc(sizeof(char) * strlen(*t2));
-    strcpy(s1, *t1);
-    strcpy(s2, *t2);
-    qsort(s1, strlen(s1), sizeof(char), cmpfunc49str);
-    qsort(s2, strlen(s2), sizeof(char), cmpfunc49str);
-    int result = strcmp(s1, s2);
-    free(s1);
-    free(s2);
-    return result;
-}
-
 char*** groupAnagrams(char** strs, int strsSize, int** columnSizes, int* returnSize) {
     //对每个单词进行sort
-    qsort(strs, strsSize, sizeof(char*), cmpfunc49Node);
-    char ***result = malloc(sizeof(char**) * strsSize);
-    int *counts = malloc(sizeof(int) * strsSize);
-    int countNum = 0;
-    int pos = 0;
-    for (int i = 0, from = 0; i <= strsSize ; i++) {
-        int r = 1;
-        if (i != strsSize && i > 0) {
-            char *s1 = malloc(sizeof(char) * strlen(strs[i]));
-            char *s2 = malloc(sizeof(char) * strlen(strs[i-1]));
-            strcpy(s1, strs[i]);
-            strcpy(s2, strs[i-1]);
-            qsort(s1, strlen(s1), sizeof(char), cmpfunc49str);
-            qsort(s2, strlen(s2), sizeof(char), cmpfunc49str);
-            r = strcmp(s1, s2);
-            free(s1);
-            free(s2);
+    int  groupCount = 0;
+    int** allKeys = NULL;
+    int* curColumnSizes = NULL;
+    char ***result = NULL;
+    
+    for (int i = 0 ; i < strsSize ; i++) {
+        //step 1,构造key数组，表示字母'a。。。'出现了几次
+        int *keys = calloc(26,sizeof(int));
+        char *cur = strs[i];
+        int length = strlen(cur);
+        for (int j = 0 ; j < length; j++) {
+            int p = cur[j] - 'a';
+            keys[p] ++;
         }
-        
-        if (i > 0 && r != 0) {
-            //from 到 i-1是相同的，构成一个char**
-            char ** tmp = malloc(sizeof(char *) * i-from);
-            int max = ((i == strsSize - 1)?i+1-from:i-from);
-            for (int j = 0 ; j < max ; j++) {
-                tmp[j] = strs[from+j];
-            }
-            if (counts == NULL) {
-                counts = malloc(sizeof(int));
-                counts[0] = i - from;
-                countNum = 1;
-            } else {
-                ++countNum;
-                counts = realloc(counts, sizeof(int) * countNum);
-                counts[countNum-1] = i - from;
-            }
-            result[pos++] = tmp;
-            from = i;
+        for (int p = 0 ; p < 26 ; p++) {
+            printf("%d ",keys[p]);
+        }
+        printf("\n");
+        if (groupCount == 0) {
+            allKeys = malloc(sizeof(int *));
+            allKeys[0] = keys;
+            
+            curColumnSizes = malloc(sizeof(int));
+            curColumnSizes[0] = 1;
+            
+            groupCount = 1;
+            
+            result = malloc(sizeof(char **));
+            result[0] = malloc(sizeof(char *));
+            result[0][0] = cur;
         } else {
-            continue;
+            int pos = 0;
+            int notMatch = 0;
+            for (int k = 0 ; k < groupCount; k++) {
+                notMatch = 0;
+                int *curK = allKeys[k];
+                for (int m = 0; m < 26; m++) {
+                    if (curK[m] != keys[m]) {
+                        notMatch = 1;
+                        break;
+                    }
+                }
+                if (notMatch) {
+                    pos++;
+                    continue;
+                } else {
+                    break;
+                }
+            }
+                
+            if (pos >= groupCount) {
+                //没找到
+                allKeys = realloc(allKeys, sizeof(int*)*(groupCount+1));
+                allKeys[groupCount] = keys;
+                
+                curColumnSizes = realloc(curColumnSizes, sizeof(int)*(groupCount+1));
+                curColumnSizes[groupCount] = 1;
+                
+                result = realloc(result, sizeof(char **)*(groupCount+1));
+                result[groupCount] = malloc(sizeof(char *));
+                result[groupCount][0] = cur;
+                groupCount++;
+            } else {
+                //找到了，第pos个
+                //key不用管了
+                curColumnSizes[pos] ++;
+                
+                result[pos] = realloc(result[pos], sizeof(char*)*curColumnSizes[pos]);
+                result[pos][curColumnSizes[pos]-1] = cur;
+            }
         }
     }
     if (returnSize) {
-        *returnSize = countNum;
+        *returnSize = groupCount;
     }
     if (columnSizes) {
-        *columnSizes = counts;
+        *columnSizes = curColumnSizes;
     }
     return result;
 }
