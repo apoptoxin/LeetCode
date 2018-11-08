96e8d5377181b3d82246f6c2175857cd3f0842de
刘楠 <liunan.coulson@bytedance.com>
2018-11-08 20:34:03 +0800

    problem 70

--- LeetCode/main.c
+++ LeetCode/main.c
@@ -8,13 +8,13 @@
 
 #include <stdio.h>
 #include "sort.h"
-#include "problem69.h"
+#include "problem70.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     
 //    bool r = isNumber(" 005047e+6");//1
 //    bool r = isNumber(".e");//1
-    int re = mySqrt(4);
+    int re = climbStairs(3);
     return 0;
 }
